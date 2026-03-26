dca8997ac01072eee9a03fca646dd61f1874f6d5
Satan <fallenangel@gmail.com>
2026-03-26 13:43:39 +0100

    1

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import './style.css'
-import * as THREE from "three";
 import { gsap } from "gsap";
 import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 import { Observer } from "gsap/observer";
@@ -10,4 +9,18 @@ import { TextPlugin } from "gsap/TextPlugin";
 
 gsap.registerPlugin(DrawSVGPlugin, Observer, ScrambleTextPlugin, ScrollTrigger, SplitText, TextPlugin);
 
-Observer.create
+const draw_timeline = gsap.timeline()
+
+draw_timeline.from(".heart_path", {
+  duration: 2,
+  drawSVG: 0,
+})
+
+// Pulse animation
+gsap.to(".heart_path", {
+  scale: 1.8,
+  transformOrigin: "50% 50%",
+  duration: 0.6,
+  repeat: -1,
+  yoyo: true,
+});
